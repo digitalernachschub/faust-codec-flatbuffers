2ea5182dc9fb1d1942b7b237fbbb139b4ddc27d5
Michael Seifert <m.seifert@digitalernachschub.de>
2019-05-13 14:20:30 +0200

    Deduplicated code for allowed field types in models.

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -21,7 +21,7 @@ _strategies_by_field_type = {
     int: integers(min_value=-2**31, max_value=2**31-1),
     UInt32: integers(min_value=0, max_value=2**32-1),
 }
-_model_fields = dictionaries(python_identifier(), sampled_from([str, int, UInt32]))
+_model_fields = dictionaries(python_identifier(), sampled_from(list(_strategies_by_field_type.keys())))
 
 
 @composite
