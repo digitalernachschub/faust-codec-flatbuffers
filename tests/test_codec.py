149272d651f85007f6af92e1ad0dad965dda5a80
Michael Seifert <m.seifert@digitalernachschub.de>
2019-06-11 08:40:45 +0200

    _to_schema_definition returns SchemaDefinition instead of str.

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -143,13 +143,13 @@ def table(draw, name=python_identifier()):
     return Table(name=name_, fields=fields_)
 
 
-def _to_schema_definition(table: Table) -> str:
+def _to_schema_definition(table: Table) -> flatc.SchemaDefinition:
     schema_definition = f'table {table.name} {{'
     for f in table.fields:
         schema_definition += f'{f.name}:{f.type};\n'
     schema_definition += '}\n'
     schema_definition += f'root_type {table.name};'
-    return schema_definition
+    return flatc.SchemaDefinition(schema_definition)
 
 
 def _to_faust_model_type(table: Table) -> Type:
@@ -171,7 +171,7 @@ def test_deserialization_reverts_serialization_when_codec_is_created_from_schema
 
     model_type = _to_faust_model_type(table_)
     model_instance = data.draw(model(model_class=just(model_type)))
-    schema = flatc.serialize_schema_definition(flatc.SchemaDefinition(schema_definition))
+    schema = flatc.serialize_schema_definition(schema_definition)
     codec = FlatbuffersCodec.from_schema(schema)
     data = model_instance.to_representation()
 
@@ -191,7 +191,7 @@ def test_dumps(data):
 
     binary = codec.dumps(data_json)
 
-    deserialized = flatc.deserialize(flatc.SchemaDefinition(schema_definition), binary)
+    deserialized = flatc.deserialize(schema_definition, binary)
     # flatc must round the floats when converting to a JSON representation
     # This means we are limited to 6 digits for float and 12 digits for double
     # see https://github.com/google/flatbuffers/issues/5371
@@ -206,7 +206,7 @@ def test_loads(data):
     model_instance = data.draw(model(model_class=just(model_type)))
     codec = FlatbuffersCodec.from_model(model_type)
     data_json = model_instance.to_representation()
-    serialized = flatc.serialize(flatc.SchemaDefinition(schema_definition), data_json)
+    serialized = flatc.serialize(schema_definition, data_json)
 
     data_deserialized = codec.loads(serialized)
 
