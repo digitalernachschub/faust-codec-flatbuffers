ec0339b56ca670dde502314ad4141fcd9a390eff
Michael Seifert <m.seifert@digitalernachschub.de>
2019-06-11 09:54:59 +0200

    The model converter test test_schema now uses generated models.

--- tests/test_faust_model_converter.py
+++ tests/test_faust_model_converter.py
@@ -1,8 +1,13 @@
 import faust
+from typing import Mapping, Type
 
-from faust_codec_flatbuffers.faust_model_converter import to_flatbuffers_schema
+from hypothesis import given
+
+from faust_codec_flatbuffers.faust_model_converter import to_flatbuffers_schema, Float64, UInt8, Int8, UInt16, Int16, UInt32, Int64, UInt64
 from faust_codec_flatbuffers.reflection.BaseType import BaseType
 from faust_codec_flatbuffers.reflection.Schema import Schema
+
+from tests.test_codec import table, _to_faust_model_type
 
 
 class Data(faust.Record):
@@ -10,20 +15,36 @@ class Data(faust.Record):
     number: int
 
 
-def test_schema():
-    model = Data
+_faust_model_type_by_flatbuffers_type: Mapping[BaseType, Type] = {
+    BaseType.UByte: UInt8,
+    BaseType.Byte: Int8,
+    BaseType.UShort: UInt16,
+    BaseType.Short: Int16,
+    BaseType.Int: int,
+    BaseType.UInt: UInt32,
+    BaseType.Long: Int64,
+    BaseType.ULong: UInt64,
+    BaseType.Float: float,
+    BaseType.Double: Float64,
+    BaseType.String: str,
+}
 
+
+@given(model=table().map(_to_faust_model_type))
+def test_schema(model):
     schema = to_flatbuffers_schema(model)
 
     assert schema.ObjectsLength() == 1
     objects = [schema.Objects(index) for index in range(schema.ObjectsLength())]
     assert schema.RootTable().Name() == objects[0].Name()
-    assert schema.RootTable().FieldsLength() == 2
+    assert schema.RootTable().FieldsLength() == len(model._options.fields)
     fields = [schema.RootTable().Fields(index) for index in range(schema.RootTable().FieldsLength())]
-    assert fields[0].Type().BaseType() == BaseType.String
-    assert fields[0].Name() == b'id'
-    assert fields[1].Type().BaseType() == BaseType.Int
-    assert fields[1].Name() == b'number'
+    for index, field in enumerate(fields):
+        field_name = field.Name().decode()
+        assert field_name in model._options.fields
+        model_field_type = model._options.fields[field_name]
+        expected_model_field_type = _faust_model_type_by_flatbuffers_type[field.Type().BaseType()]
+        assert expected_model_field_type == model_field_type
 
 
 def test_schema_corresponds_to_reference():
